111e82593a7af1071be8f4dbdeb5d45caba5aa7a
Jeswin Kumar <jeswinpk@agilehead.com>
2018-07-18 20:52:59 +0530

    WIP

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,22 @@ import * as pool from "./pool";
 
 export { default as setup } from "./setup";
 
+export interface ILinkEntry {
+  title: string;
+  link: string;
+  description?: string;
+}
+
+export interface ILinks {
+  items: ILinkEntry[];
+}
+
+export async function getLinks(section: string, username: string) {
+  return {
+    items: []
+  } as ILinks;
+}
+
 export async function init(dbConfig: IDbConfig) {
   return {
     pool: await pool.init(dbConfig)
